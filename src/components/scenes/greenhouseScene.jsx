8d04da70f09af8b05d035d1366ebff2f71c8f0ff
tomlukewarden <tomlukeprogramming@outlook.com>
2025-06-10 14:42:29 +0100

    Refactor GameCanvas to remove StartScene and update scene configuration; enhance GreenhouseScene by adding character controls and loading assets

--- src/components/scenes/greenhouseScene.jsx
+++ src/components/scenes/greenhouseScene.jsx
@@ -8,6 +8,7 @@ constructor(){
 preload(){
 
     this.load.image("greenhouseBackground", "src/assets/backgrounds/greenhouse.png");
+    this.load.image("defaultFront", "src/assets/char/default/front-default.png");
 }
 
 create(){
@@ -15,6 +16,28 @@ create(){
     const { width, height } = this.sys.game.config;
 
     this.add.image(width / 2, height / 2, "greenhouseBackground").setScale(0.225);
+   const char = this.add.image(width / 2, height / 2, "defaultFront").setScale(0.06);
+    this.input.keyboard.on("keydown", (event) => {
+        switch (event.key) {
+            case "w":
+                char.y -= 10;
+                break;
+            case "s":
+                char.y += 10;
+                break;
+            case "a":
+                char.x -= 10;
+                break;
+            case "d":
+                char.x += 10;
+                break;
+            default:
+                break;
+        }
+    }
+    );
+
+
 }
 
 }
