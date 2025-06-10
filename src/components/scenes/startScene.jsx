60caf51576a4f586deb0d1d018721fdefb4cfed2
tomlukewarden <tomlukeprogramming@outlook.com>
2025-06-10 12:32:06 +0100

    Refactor StartScene create method for improved readability and organization and interaction on click to next scene

--- src/components/scenes/startScene.jsx
+++ src/components/scenes/startScene.jsx
@@ -12,12 +12,27 @@ class StartScene extends Phaser.Scene {
 
     create() {
         const { width, height } = this.sys.game.config;
+        this.cameras.main.setBackgroundColor("#b36f4b");
 
-      this.add.image(width / 2, height / 3, "logo").setScale(0.35);
-      this.add.image(width / 2, height / 1.3, "startButton").setScale(0.3);
-      this.cameras.main.setBackgroundColor("#b36f4b");
+      const logo = this.add.image(width / 2, height / 3, "logo").setScale(0.35);
+      const startButton = this.add.image(width / 2, height / 1.3, "startButton").setScale(0.3).setInteractive();
+      startButton.on("pointerdown", () => {
+          this.scene.start("GameScene");
+      });
+      this.add.text(width / 2, height / 1.1, "Click to Start", {
+          fontSize: "24px",
+          fill: "#fff"
+      }).setOrigin(0.5);
+      this.add.text(width / 2, height / 1.05, "Made by Thomas Warden | Art by Emma Formosa", {
+          fontSize: "16px",
+          fill: "#fff"
+      }).setOrigin(0.5);
+      this.add.text(width / 2, height / 1.02, "Demo: 1.0.0", {
+          fontSize: "16px",
+          fill: "#fff"
+      }).setOrigin(0.5);
 
-
+    
     }
 }
 
