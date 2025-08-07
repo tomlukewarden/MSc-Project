c7375e5cfc8c928a40335c4f4ca6fb219c86cef0
tomlukewarden <tomlukeprogramming@outlook.com>
2025-08-07 16:20:40 +0100

    Enhance MovementTutorial with character creation and obstacle collision detection

--- src/tutorials/movementTutorial.jsx
+++ src/tutorials/movementTutorial.jsx
@@ -1,4 +1,5 @@
 import Phaser from 'phaser';
+import { createMainChar } from '../characters/mainChar';
 
 class MovementTutorial extends Phaser.Scene {
   constructor() {
@@ -6,13 +7,26 @@ class MovementTutorial extends Phaser.Scene {
     this.player = null;
     this.cursors = null;
     this.obstacles = [];
+    this.collisionGroup = null;
   }
 
   preload() {
     this.load.image('personalGardenBg', '/assets/backgrounds/personal/personalBackground.png');
-    this.load.image('player', '/assets/characters/player.png');
     this.load.image('rock', '/assets/misc/rock.png');
     this.load.image('tree', '/assets/misc/tree.png');
+    this.load.image("defaultFront", "/assets/char/default/front-default.png");
+    this.load.image("defaultBack", "/assets/char/default/back-default.png");
+    this.load.image("defaultLeft", "/assets/char/default/left-default.png");
+    this.load.image("defaultRight", "/assets/char/default/right-default.png");
+    this.load.image("defaultFrontWalk1", "/assets/char/default/front-step-1.PNG");
+    this.load.image("defaultFrontWalk2", "/assets/char/default/front-step-2.PNG");
+    this.load.image("defaultBackWalk1", "/assets/char/default/back-step-1.PNG");
+    this.load.image("defaultBackWalk2", "/assets/char/default/back-step-2.PNG");
+    this.load.image("defaultLeftWalk1", "/assets/char/default/left-step-1.PNG");
+    this.load.image("defaultLeftWalk2", "/assets/char/default/left-step-2.PNG");
+    this.load.image("defaultRightWalk1", "/assets/char/default/right-step-1.PNG");
+    this.load.image("defaultRightWalk2", "/assets/char/default/right-step-2.PNG");
+    this.load
   }
 
   create() {
@@ -27,39 +41,28 @@ class MovementTutorial extends Phaser.Scene {
     this.add.rectangle(width / 2, 80, 600, 60, 0xffffff, 0.85)
       .setStrokeStyle(2, 0x228B22)
       .setDepth(1);
-    this.add.text(width / 2, 80, 'Use arrow keys or WASD to move. Avoid obstacles!', {
+    this.add.text(width / 2, 80, 'Use WASD to move. Avoid obstacles!', {
       fontFamily: 'Georgia',
       fontSize: '24px',
       color: '#228B22'
     }).setOrigin(0.5).setDepth(2);
 
-    // Player
-    this.player = this.physics.add.sprite(width / 2, height / 2 + 100, 'player')
-      .setScale(0.13)
-      .setCollideWorldBounds(true);
+    // Obstacles group
+    this.collisionGroup = this.physics.add.staticGroup();
+    const rock1 = this.physics.add.staticImage(width / 2 - 150, height / 2, 'rock').setScale(2).setAlpha(1).setDepth(100);
+    const tree = this.physics.add.staticImage(width / 2 + 120, height / 2 - 60, 'tree').setScale(4).setAlpha(1).setDepth(100);
+    const rock2 = this.physics.add.staticImage(width / 2 + 40, height / 2 + 80, 'rock').setScale(2).setAlpha(1).setDepth(100);
+    this.collisionGroup.add(rock1);
+    this.collisionGroup.add(tree);
+    this.collisionGroup.add(rock2);
 
-    // Obstacles
-    this.obstacles = [
-      this.physics.add.staticImage(width / 2 - 150, height / 2, 'rock').setScale(0.18),
-      this.physics.add.staticImage(width / 2 + 120, height / 2 - 60, 'tree').setScale(0.22),
-      this.physics.add.staticImage(width / 2 + 40, height / 2 + 80, 'rock').setScale(0.15)
-    ];
+    // Create main character with animations and collisions
+    this.player = createMainChar(this, width, height, 0.04, this.collisionGroup);
 
-    // Enable collision
-    this.obstacles.forEach(ob => {
-      this.physics.add.collider(this.player, ob, () => {
-        this.player.setTint(0xff4444);
-        this.time.delayedCall(200, () => this.player.clearTint());
-      });
-    });
-
-    // Controls
-    this.cursors = this.input.keyboard.createCursorKeys();
-    this.wasd = this.input.keyboard.addKeys({
-      up: Phaser.Input.Keyboard.KeyCodes.W,
-      left: Phaser.Input.Keyboard.KeyCodes.A,
-      down: Phaser.Input.Keyboard.KeyCodes.S,
-      right: Phaser.Input.Keyboard.KeyCodes.D
+    // Collision feedback (red flash)
+    this.physics.add.collider(this.player, this.collisionGroup, () => {
+      this.player.setTint(0xff4444);
+      this.time.delayedCall(200, () => this.player.clearTint());
     });
 
     // Back button
@@ -77,20 +80,6 @@ class MovementTutorial extends Phaser.Scene {
       .on("pointerdown", () => {
         this.scene.start("PersonalGarden");
       });
-  }
-
-  update() {
-    if (!this.player) return;
-    const speed = 180;
-    let vx = 0, vy = 0;
-
-    if (this.cursors.left.isDown || this.wasd.left.isDown) vx = -speed;
-    else if (this.cursors.right.isDown || this.wasd.right.isDown) vx = speed;
-
-    if (this.cursors.up.isDown || this.wasd.up.isDown) vy = -speed;
-    else if (this.cursors.down.isDown || this.wasd.down.isDown) vy = speed;
-
-    this.player.setVelocity(vx, vy);
   }
 }
 
