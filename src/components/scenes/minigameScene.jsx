e496863699116d2940f60d8367cdbd23294caacc
tomlukewarden <tomlukeprogramming@outlook.com>
2025-08-19 18:52:32 +0100

    Refactor code structure for improved readability and maintainability

--- src/components/scenes/minigameScene.jsx
+++ src/components/scenes/minigameScene.jsx
@@ -10,7 +10,7 @@ class MiniGameScene extends Phaser.Scene {
     this.cameras.main.setBackgroundColor("#b3e6ff");
 
 
-    const minigames = [ "XOTutorialScene", "FishTutorialScene", "FlowerCatchTutorial"]; 
+    const minigames = [ "XOTutorialScene","FishTutorialScene", "FlowerCatchTutorial"]; 
     // Pick a random minigame
     const chosenMinigame = minigames[Math.floor(Math.random() * minigames.length)];
 
